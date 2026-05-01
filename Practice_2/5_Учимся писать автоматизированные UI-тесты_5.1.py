0b9769bedd62e2a34d9119fefcc8e0a048a53788
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-05-01 12:01:50 +0300

    1

--- Practice_2/5_Учимся писать автоматизированные UI-тесты_5.1.py
+++ Practice_2/5_Учимся писать автоматизированные UI-тесты_5.1.py
@@ -1,29 +1,48 @@
-from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.maximize_window()
-driver.get("https://ya.ru/")
+
+driver.get("https://ya.ru/") #браузер откроет страницу
+driver.get("https://vk.com/") #перейдет на следующую страницу
+driver.set_window_size(640, 460) #окно браузера уменьшится под параметры
+#driver.maximize_window #открыть окно по размеру экрана
+#driver.minimize_window #свернуть окно браузера
+#driver.fullscreen_window #развернуть окно на весь экран, аналог F11
+#driver.back() #назад
+#driver.forward() #Вперед
+#driver.refresh() #Обновить
+
+
 sleep(3)
 
-driver.fullscreen_window()
-
-sleep(3)
-
-driver.save_screenshot("./ya.png")
+driver.save_screenshot('./ya.png')
 
 
 
-#driver.get("https://vk.com")
 
 
-#for x in range(1, 10):
-#    driver.back()
-#    driver.forward()
-#    driver.refresh()
 
-#driver.set_window_size(640, 460)
 
-sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Practice_2/5_Учимся писать автоматизированные UI-тесты_5.2.py
+++ Practice_2/5_Учимся писать автоматизированные UI-тесты_5.2.py
@@ -1,56 +1,47 @@
-from time import sleep
+from selenium import webdriver
+
+driver = webdriver.Chrome()
+
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
+from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-driver.maximize_window()
 
-#Заити в лабиринт
 driver.get("https://www.labirint.ru/")
 
+search_field = "#search-field" #мы уже создали переменную со значением локатора
+search_input = driver.find_element(By.CSS_SELECTOR, search_field) #помещаем переменную
+search_input.send_keys('Python') #отправить значение Python в поисковую строку
+search_input.send_keys(Keys.RETURN) #выполнить поиск./ для компактной клавиатуры, как на MacBook
+product_card = 'div.product-card'
 
-
-#Наити книги по слову Python
-search_locator = '#search-field'
-
-search_input = driver.find_element(By.CSS_SELECTOR, search_locator)
-search_input.send_keys('Python')
-search_input.send_keys(Keys.RETURN)
-#2.Способ через зяпятую: search_input.send_keys('Python', Keys.RETURN)
-
-
-#Собрать все карточки товаров
-books = driver.find_elements(By.CSS_SELECTOR, 'div.product-card, document')
+#собрать все карточки товаров(количество)
+books = driver.find_elements(By.CSS_SELECTOR, product_card)
 print(len(books))
 
+#for book in books:
+#    print(book.text) #Вся информация о книгах
 
-#Вывести в консоль инфу : название + аквтор + цена
+#for book in books: #(находим по автору все 60 книг , но в данной проверке 60 книг но названы втором только 59 книг)
+#	author = book.find_element(By.CSS_SELECTOR, '.product-card__author').text
+#	print(author)
+
 for book in books:
-    title = book.find_element(By.CSS_SELECTOR, "a.product-card__name").text
-    price = book.find_element(By.CSS_SELECTOR, "div.product-card__price-current").text
-    author = ''
-try: #Python сам попробует определить значение
-    author = book.find_element(By.CSS_SELECTOR, 'div.product-card__author').text
-except: #если не получится, то значение мы задали сами
-    author = 'Автор не указан'
-
-print(author + '\t' + title + '\t' + price)
-
+	title = book.find_element(By.CSS_SELECTOR, '.product-card__name').text
+	price = book.find_element(By.CSS_SELECTOR, '.product-card__price-current').text
+	author = " "
+	try:
+		author = book.find_element(By.CSS_SELECTOR, '.product-card__author').text
+	except:
+		author = "Автор не указан"
+	print(author + "\t" + title + "\t" + price)
 
 
-#product-card__author
-
-sleep(5)
-
-
-
-
-
-
-
+sleep(3)
 
 
 
