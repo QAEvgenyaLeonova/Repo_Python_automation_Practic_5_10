362f89768ce67fe58a0b205919fe7703ad6ac12f
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-05-05 09:52:20 +0300

    4

---
+++ 6_Расширенные практики Selenium_6.0/1_ya.py
@@ -0,0 +1,23 @@
+from pydoc import browse
+from time import sleep
+from selenium import webdriver
+#две строки ниже можно оставить, они не помешают работе
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
+#три строки ниже - новые, импорт драйвера для Firefox
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+#Импортируем для Edge
+from selenium.webdriver.edge.service import Service as EdgeService
+from webdriver_manager.microsoft import EdgeChromiumDriverManager
+
+#browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+#browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+browser = webdriver.Edge(service=EdgeService(EdgeChromiumDriverManager().install()))
+
+browser.maximize_window() #для разворачивания окна
+browser.get("https://ya.ru/") #для перехода на нужную страницу
+sleep(5) #для паузы на загрузку контента страницы
+
+browser.save_screenshot("./ya.png") #для сохранения скриншота
+browser.quit() #для закрытия окна
