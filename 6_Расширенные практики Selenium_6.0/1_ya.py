aca81b8d776ec7f3ddaaccaf497e7cd5b958dac6
Evgenya-odyvanchik <Eva.new@inbox.ru>
2026-05-18 21:35:31 +0300

    7

--- 6_Расширенные практики Selenium_6.0/1_ya.py
+++ 6_Расширенные практики Selenium_6.0/1_ya.py
@@ -11,9 +11,9 @@ from  selenium.webdriver.edge.service  import  Service  as  EdgeService
 from  webdriver_manager.microsoft  import  EdgeChromiumDriverManager
 
 browser = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-#browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-#edge_driver_path = "C:\\Users\\user\\Documents\\2.НЕ ПЕРЕКИНУТОЕ\\5.Автоматизация тестирования на Python\\Урок_1. Знакомство с языком Python\\Эдже_драйвер\\msedgedriver.exe"
-#browser = webdriver.Edge(service=EdgeService(executable_path=edge_driver_path))
+browser = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+edge_driver_path = "C:\\Users\\user\\Documents\\2.НЕ ПЕРЕКИНУТОЕ\\5.Автоматизация тестирования на Python\\Урок_1. Знакомство с языком Python\\Эдже_драйвер\\msedgedriver.exe"
+browser = webdriver.Edge(service=EdgeService(executable_path=edge_driver_path))
 
 
 browser.maximize_window()  # для разворачивания окна
